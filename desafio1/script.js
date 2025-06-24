343cba977dfa4d604107029df34c4299287fad2e
AndreJacobino <andre.jacobino@aluno.senai.br>
2025-06-24 11:06:45 -0300

    iniciante

--- desafio1/script.js
+++ desafio1/script.js
@@ -4,6 +4,7 @@ const resultado = document.getElementById("resul")
 
 envia.addEventListener("click", function (){
     let numero = parseInt(num.value)
+    resultado.innerHTML = "";
     for (let i = 1; i <= 10; i++) {
 
         let  resultado = numero *i 
